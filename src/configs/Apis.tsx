14f9ff9ad9b5eb2f5141a3c702301baaf0ecaa53
NguyenHuyTu <tu092002@gmail.com>
2025-07-10 15:22:12 +0700

    tu update change BrouserRouter -> HashRouter

--- src/configs/Apis.tsx
+++ src/configs/Apis.tsx
@@ -2,54 +2,55 @@ import axios from "axios";
 import { Cookies } from "react-cookie";
 
 const SERVER_CONTEXT = "";
-export const SERVER = "http://localhost:3966/api/v1";
+// export const SERVER = "http://localhost:3966/api/v1";
+export const SERVER = "http://118.69.168.36:3966/api/v1";
 
 // Định nghĩa các endpoint với SERVER_CONTEXT
 export const endpoints = {
-    // Home
-    APICarousel: `${SERVER_CONTEXT}/home/carousel`,
-    APIIntroduction: `${SERVER_CONTEXT}/home/introduction`,
-    APIDichvu: `${SERVER_CONTEXT}/home/servicelist`,
-    APINews: `${SERVER_CONTEXT}/home/news`,
-    APIHeader: `${SERVER_CONTEXT}/header`,
-    APIFooter: `${SERVER_CONTEXT}/footer`,
+  // Home
+  APICarousel: `${SERVER_CONTEXT}/home/carousel`,
+  APIIntroduction: `${SERVER_CONTEXT}/home/introduction`,
+  APIDichvu: `${SERVER_CONTEXT}/home/servicelist`,
+  APINews: `${SERVER_CONTEXT}/home/news`,
+  APIHeader: `${SERVER_CONTEXT}/header`,
+  APIFooter: `${SERVER_CONTEXT}/footer`,
 
-    // Navigator
-    APIHoaTieu: `${SERVER_CONTEXT}/hoatieu`,
+  // Navigator
+  APIHoaTieu: `${SERVER_CONTEXT}/hoatieu`,
 
-    // Kế hoạch dẫn tàu sơ cua
-    APIKehoachdantau: `${SERVER_CONTEXT}/kehoachdantau`,
-    APISwitch: `${SERVER_CONTEXT}/switch`,
+  // Kế hoạch dẫn tàu sơ cua
+  APIKehoachdantau: `${SERVER_CONTEXT}/kehoachdantau`,
+  APISwitch: `${SERVER_CONTEXT}/switch`,
 
-    // Maneuvering Draft
-    APIManeuveringDraft: `${SERVER_CONTEXT}/maneuvering-draft`,
+  // Maneuvering Draft
+  APIManeuveringDraft: `${SERVER_CONTEXT}/maneuvering-draft`,
 
-    // Service Price
-    APIServicePrice: `${SERVER_CONTEXT}/product-price`,
+  // Service Price
+  APIServicePrice: `${SERVER_CONTEXT}/product-price`,
 
-    // Ship
-    APIShip: `${SERVER_CONTEXT}/ship`,
+  // Ship
+  APIShip: `${SERVER_CONTEXT}/ship`,
 
-    // Stream
-    APIStream: `${SERVER_CONTEXT}/stream`,
+  // Stream
+  APIStream: `${SERVER_CONTEXT}/stream`,
 
-    // Tide Calendar
-    APITide: `${SERVER_CONTEXT}/tide-calendar`,
+  // Tide Calendar
+  APITide: `${SERVER_CONTEXT}/tide-calendar`,
 
-    // Items
-    APIItems: `${SERVER_CONTEXT}/items`,
+  // Items
+  APIItems: `${SERVER_CONTEXT}/items`,
 
-    // Authentication & Users
-    APILogin: `${SERVER_CONTEXT}/login`,
-    APICurrentUser: `${SERVER_CONTEXT}/current-user`,
-    APIRegister: `${SERVER_CONTEXT}/users`,
+  // Authentication & Users
+  APILogin: `${SERVER_CONTEXT}/login`,
+  APICurrentUser: `${SERVER_CONTEXT}/current-user`,
+  APIRegister: `${SERVER_CONTEXT}/users`,
 
-    // Posts & Comments
-    APIDetails: (idPost: number) => `${SERVER_CONTEXT}/posts/${idPost}/`,
-    APIComments: (idPost: number) => `${SERVER_CONTEXT}/posts/${idPost}/comments/`,
-    APIAddComment: `${SERVER_CONTEXT}/comments/`,
+  // Posts & Comments
+  APIDetails: (idPost: number) => `${SERVER_CONTEXT}/posts/${idPost}/`,
+  APIComments: (idPost: number) =>
+    `${SERVER_CONTEXT}/posts/${idPost}/comments/`,
+  APIAddComment: `${SERVER_CONTEXT}/comments/`,
 };
-
 
 // Lấy token từ cookies
 const cookies = new Cookies();
@@ -57,13 +58,13 @@ const token = cookies.get("token");
 
 // Tạo axios instance có sẵn Authorization nếu có token
 export const authApi = axios.create({
-    baseURL: SERVER,
-    headers: token ? { Authorization: `Bearer ${token}` } : {},
+  baseURL: SERVER,
+  headers: token ? { Authorization: `Bearer ${token}` } : {},
 });
 
 // Axios mặc định không có token
 const api = axios.create({
-    baseURL: SERVER,
+  baseURL: SERVER,
 });
 
 export default api;
